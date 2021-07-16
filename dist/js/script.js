5e2b4e981cd364b76bd454ae25ace31408d07331
kukuvz <antonslayer@gmail.com>
2021-07-16 11:02:46 +0300

    adaptive footer

--- dist/js/script.js
+++ dist/js/script.js
@@ -97,9 +97,14 @@ $(function () {
         ratedFill: "#1C62CD",
         spacing: "7px",
         normalFill: "#C4C4C4"
-      });
+    });
 
-      $('.menu__btn').on('click', function() {
+    $('.menu__btn').on('click', function() {
         $('.menu-mobile__list').toggleClass('menu-mobile__list--active')
-      });
+    });
+
+    $('.footer__topdrop').on('click', function() {
+        $(this).next().slideToggle();
+        $(this).toggleClass('footer__topdrop--active');
+    });
 });
